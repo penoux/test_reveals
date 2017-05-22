83843201f55239632c69a73447030750f11a903e
Patrick Enoux <patrick.enoux@orange.com>
2017-05-22 16:17:34 +0200

    Initial commit

---
+++ source/_ext/special.rst
@@ -0,0 +1,17 @@
+
+.. Some new roles (+ fragments.js) added to allow lists with fragments without raw html
+
+.. role:: fr-grow
+.. role:: fr-shrink
+.. role:: fr-fade-out
+.. role:: fr-fade-up
+.. role:: fr-fade-down
+.. role:: fr-fade-left
+.. role:: fr-fade-right
+.. role:: fr-current-visible
+.. role:: fr-highlight-current-red
+.. role:: fr-highlight-current-green
+.. role:: fr-highlight-current-blue
+.. role:: fr-highlight-red
+.. role:: fr-highlight-green
+.. role:: fr-highlight-blue
---
+++ source/_static/fragments.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$(".fr-grow").parent().addClass("fragment grow");
+	$(".fr-shrink").parent().addClass("fragment shrink");
+	$(".fr-fade-out").parent().addClass("fragment fade-out");
+	$(".fr-fade-up").parent().addClass("fragment fade-up");
+	$(".fr-fade-down").parent().addClass("fragment fade-down");
+	$(".fr-fade-left").parent().addClass("fragment fade-left");
+	$(".fr-fade-right").parent().addClass("fragment fade-right");
+	$(".fr-current-visible").parent().addClass("fragment current-visible");
+	$(".fr-highlight-current-red").parent().addClass("fragment highlight-current-red");
+	$(".fr-highlight-current-green").parent().addClass("fragment highlight-current-green");
+	$(".fr-highlight-current-blue").parent().addClass("fragment highlight-current-blue");
+	$(".fr-highlight-red").parent().addClass("fragment highlight-red");
+	$(".fr-highlight-green").parent().addClass("fragment highlight-green");
+	$(".fr-highlight-blue").parent().addClass("fragment highlight-blue");
+});
